8d008f988200ab3ea7561a4d9402831f9c681ab8
shmelev <sergey.shmelev@loginom.ru>
2020-08-11 09:59:14 +0300

    Первичная загрузка.

---
+++ account_manager.py
@@ -0,0 +1,212 @@
+import sqlite3
+import os.path
+import pandas as pd
+import re
+import numpy as np
+
+class Account():
+    
+    MESSAGES = {'already_exist': 'Ваш аккаунт уже существует.',
+                'created': 'Ваш аккаунт создан.',
+                'expense_added': 'Запись добавлена.',
+                'error_expense_add': 'Ошибка: запись не добавлена.',
+                'account_link_add': 'Ссылка на аккаунт добавлена.',
+                'account_link_delete': 'Ссылка на аккаунт удалена.',
+                'account_link_error': 'Ошибка: действие не выполнено.'
+               }
+    
+    MODIFER_FIELDS = {'get_date' : ['Дата'], 
+                    'get_category' : ['Категория'],
+                    'get_linked' : ['Аккаунт'],
+                    'get_all' : None
+                     }
+    
+    def __init__(self, user_id):
+        self.user_id = user_id
+        
+    def check(self):
+        if os.path.exists('.\data\{}.db'.format(self.user_id)):
+            return self.MESSAGES['already_exist']
+        else:
+            conn = sqlite3.connect('.\data\{}.db'.format(self.user_id))
+            c = conn.cursor()
+            c.execute('''CREATE TABLE finance (            
+                            date     DATETIME,
+                            category VARCHAR (100),
+                            expenses INTEGER,
+                            description     VARCHAR (100)
+                        );''')
+            c.execute('''CREATE TABLE config (
+                            parameter VARCHAR (100),
+                            value     VARCHAR (100) 
+                        );''')
+            conn.commit()
+            conn.close()        
+            return self.MESSAGES['created']
+        
+    def add_expense(self, data):
+        try:
+            conn = sqlite3.connect('.\data\{}.db'.format(self.user_id))
+            c = conn.cursor()
+            c.execute('''INSERT INTO finance (
+                                date,
+                                category,
+                                expenses,
+                                description
+                            )
+                            VALUES (
+                                '{0}',
+                                '{1}',
+                                '{2}',
+                                '{3}'
+                            );'''.format(
+                                         data[0], 
+                                         data[1], 
+                                         data[2], 
+                                         data[3]
+                                        ))
+            conn.commit()
+            conn.close()
+            return self.MESSAGES['expense_added']
+        except: 
+            return self.MESSAGES['error_expense_add']
+        
+        
+    def get_expense(self, modifers):
+        result = []
+        conn = sqlite3.connect('.\data\{}.db'.format(self.user_id))
+        c = conn.cursor()
+        for row in c.execute('''SELECT date, category, expenses, description
+                                        FROM finance
+                                        order by date DESC, category ASC, expenses DESC'''):
+            date_data = str(row[0])
+            category_data = str(row[1])
+            expense_data = row[2]    
+            description_data = row[3]
+            result.append([
+                            date_data,
+                            category_data,
+                            expense_data,
+                            description_data])
+        conn.close()
+        df = pd.DataFrame(result, columns=['Дата', 'Категория', 'Расходы', 'Описание'])
+        
+        modifers_final = []
+        if type(modifers) == str:
+            modifers_final = [modifers]
+        elif type(modifers) == list:
+            for modifer in modifers:
+                try:
+                    modifers_final.append(self.MODIFER_FIELDS[modifer][0])
+                except:
+                    continue
+        if len(modifers_final) == 0:
+            modifers_final = None 
+        
+        if modifers_final != None and modifers_final != ['get_linked']:
+            df = pd.pivot_table(df, values='Расходы', index = modifers_final,
+                        aggfunc=np.sum)
+            df_string = df.to_string()
+        else:
+            df_string = df.to_string(index = False, justify = 'left')
+            df_string = re.sub('\n\s+', '\n', df_string)
+                                
+        df_string_corrected = '`' + df_string + '`'
+        return df_string_corrected
+    
+    def account_link(self, linked_account, mode):
+        if mode == 'add':
+            try:
+                conn = sqlite3.connect('.\data\{}.db'.format(self.user_id))
+                c = conn.cursor()
+                c.execute('''INSERT INTO config (
+                                        parameter,
+                                        value
+                                    )
+                                    VALUES (
+                                        '{0}',
+                                        '{1}'
+                                    );'''.format(
+                                                 'linked_account', 
+                                                 linked_account
+                                                ))
+                conn.commit()
+                conn.close()
+                return self.MESSAGES['account_link_add']
+            except: 
+                return self.MESSAGES['account_link_error']
+        elif mode == 'delete':
+            try:
+                conn = sqlite3.connect('.\data\{}.db'.format(self.user_id))
+                c = conn.cursor()
+                c.execute('''DELETE FROM config 
+                                    WHERE
+                                    parameter = '{0}' AND
+                                    value = '{1}'
+                                    ;'''.format(
+                                                 'linked_account', 
+                                                 linked_account
+                                                ))
+                conn.commit()
+                conn.close()
+                return self.MESSAGES['account_link_delete']
+            except: 
+                return self.MESSAGES['account_link_error']
+        else:
+            return self.MESSAGES['account_link_error']        
+        
+    def get_all_expense(self, modifers):
+        
+        linked_accounts = [self.user_id]
+        result = []
+        
+        conn = sqlite3.connect('.\data\{}.db'.format(self.user_id))
+        c = conn.cursor()        
+        for row in c.execute('''SELECT DISTINCT value
+                                        FROM config
+                                        WHERE parameter = 'linked_account'  '''):
+            linked_accounts.append(str(row[0]))
+        conn.close()        
+        
+        for account in linked_accounts:
+            conn = sqlite3.connect('.\data\{}.db'.format(account))
+            c = conn.cursor()
+            for row in c.execute('''SELECT date, category, expenses, description, '{}' as account
+                                            FROM finance
+                                            order by date DESC, category ASC, expenses DESC'''.format(account)):
+                date_data = str(row[0])
+                category_data = str(row[1])
+                expense_data = row[2]    
+                description_data = row[3]
+                account_data = row[4]
+                result.append([
+                                date_data,
+                                category_data,
+                                expense_data,
+                                description_data,
+                                account_data])
+        conn.close()
+        df = pd.DataFrame(result, columns=['Дата', 'Категория', 'Расходы', 'Описание', 'Аккаунт'])
+        
+        modifers_final = []
+        if type(modifers) == str:
+            modifers_final = [modifers]
+        elif type(modifers) == list:
+            for modifer in modifers:
+                try:
+                    modifers_final.append(self.MODIFER_FIELDS[modifer][0])
+                except:
+                    continue
+        if len(modifers_final) == 0:
+            modifers_final = None   
+        
+        if modifers_final != None and modifers_final != ['Аккаунт']:
+            df = pd.pivot_table(df, values='Расходы', index = modifers_final,
+                        aggfunc=np.sum)
+            df_string = df.to_string()
+        else:
+            df_string = df.to_string(index = False, justify = 'left')
+            df_string = re.sub('\n\s+', '\n', df_string)
+                                
+        df_string_corrected = '`' + df_string + '`'
+        return df_string_corrected
---
+++ message_analyzer.py
@@ -0,0 +1,113 @@
+import re
+from datetime import datetime
+import pandas as pd
+
+class Analyzer():
+    
+    IGNORE_LIST = ['рублей','руб','р']
+    WELCOME_LIST = ['привет','хай','добрый день','здравствуйте']
+    GET_LIST = ['получить','показать','затраты','охуеть', 'посмотреть', 'вывести','показать по категории', 'показать по дате', 'показать по дате и категории', 'показать по категории и дате']
+    CLEAR_LIST = ['очистить','удалить','стереть']
+    
+    CATEGORY_LIST = {'продукты' : ['пятерочка','лента','перекресток','продукты','еда','корм'], 
+                     'автомобиль' : ['бензин','т/о','запчасти','автомобиль','бенз'],
+                     'развлечения' : ['роллы','пиво','рейв','флекс','туса','кафе','бар','игги','томато','дудки','баня','велосипеды','развлечения'],
+                     'красота' : ['подружка','косметика','красота','косметолог']
+                    }
+    
+    MODIFER_LIST = {'get_date' : ['дате','дату','датам'], 
+                    'get_category' : ['категории', 'группе', 'категориям', 'группам'],
+                    'get_all' : ['все','всё','полностью'],
+                    'get_linked_account' : ['связанные','доступные']
+                   }
+    
+    MESSAGES = {'mode_data_recording': 'Запись расходов.',
+                'mode_command_processing': 'Обработка команды.',
+                'mode_command_processing_welcome': 'Приветствие.',
+                'mode_command_processing_get': 'Получение данных.',
+                'mode_command_processing_clear': 'Удаление данных.',
+                'identified_categories': 'Выделены категории: ',
+                'error_several_categories': 'Ошибка: определено несколько категорий.',
+                'target_category': 'Категория определена: ',
+                'error_invalid_input_data': 'Ошибка: некорректные входные данные.',
+                'error_invalid_command': 'Ошибка: некорректная команда.',
+                'error_category_detector': 'Ошибка: категория не определена.'
+               }
+    
+    # определяет категорию расходов по ключевому слову
+    def category_detector(self, input_word):
+        for category in self.CATEGORY_LIST:
+            for key_word in self.CATEGORY_LIST[category]:
+                if key_word == input_word:
+                    return category
+        return ValueError
+    
+    # определяет категорию расходов по ключевому слову
+    def modifer_detector(self, input_word):
+        for modifer in self.MODIFER_LIST:
+            for key_word in self.MODIFER_LIST[modifer]:
+                if key_word == input_word:
+                    return modifer
+        return ValueError
+    
+    '''
+    Вход: сообщение пользователя
+    Выходы: сообщение, данные. Если ошибка, то в данных ValueError
+    '''
+    def message_analyzer(self, string):
+        result = []
+        data = []
+        date = str(datetime.now().date()) # текущая дата
+        find_digit = re.findall(r'\d+', string) # поиск чисел
+        find_text = re.findall(r'[A-zА-я]+', string) # поиск слов
+        if len(find_digit)>0 and len(find_text)>0: # если есть и числа и слова        
+            result.append(self.MESSAGES['mode_data_recording']) # print('Режим 1. Запись расходов.')
+            category_list = [] # хранилище под найденные категории
+            expense = find_digit[0] # первое выделенное число (аналируем только его, все остальные идут в описание)
+            for word in find_text: 
+                word = word.strip().lower()
+                category = self.category_detector(word) # определяем категорию каждого слова
+                if category != ValueError: # если категория опредеена, то добавляем в список
+                    category_list.append(category)
+            if len(category_list)>0: # если категории удалось выделить
+                result.append(self.MESSAGES['identified_categories'] + ', '.join(category_list) + '.') # print('Выделены категории: ', category_list)
+                if len(set(category_list)) != 1: # выдать ошибку если выделено более одной
+                    result.append(self.MESSAGES['error_several_categories']) # print('Определено несколько категорий.')
+                    return result, ValueError, 'error'
+                else: # если выделена одна, то сформировать список для записи в БД
+                    category = category_list[0] # выделенная категория
+                    description = ' '.join([word for word in find_text if word not in set(self.IGNORE_LIST)]) # описание - выделенные слова без IGNORE_LIST-слов
+                    result.append(self.MESSAGES['target_category'] + category_list[0] + '.') # print('Категория определена: ', category_list[0])
+                    data.append([date, category, expense, description]) # print([expense, category, date, description]) # затраты, категория, дата, описание
+                    return result, data, 'add'
+            else:
+                result.append(self.MESSAGES['error_category_detector'])
+                return result, ValueError, 'error'
+        elif len(find_text)>0: # если есть толлько текст
+            result.append(self.MESSAGES['mode_command_processing']) # print('Режим 2. Обработка команды.')
+            for word in find_text:
+                word = word.strip().lower()
+                if word in self.WELCOME_LIST:
+                    result.append(self.MESSAGES['mode_command_processing_welcome']) # print('Режим 2.1. Приветствие.')
+                    return result, data, 'welcome'
+                elif word in self.GET_LIST:
+                    result.append(self.MESSAGES['mode_command_processing_get']) # print('Режим 2.2. Получение данных.')
+                    modifer_list = [] # хранилище под найденные модификаторы
+                    for word in find_text: 
+                        word = word.strip().lower()
+                        modifer = self.modifer_detector(word) # определяем модификатор каждого слова
+                        if modifer != ValueError: # если модификатор опредеен, то добавляем в список
+                            modifer_list.append(modifer)
+                    if len(modifer_list) == 0:
+                        modifer_list.append('get_all')
+                    get = modifer_list #[0]
+                    return result, data, get
+                elif word in self.CLEAR_LIST:
+                    result.append(self.MESSAGES['mode_command_processing_clear']) # print('Режим 2.3. Удаление данных.')
+                    return result, data, 'clear'
+                else:
+                    result.append(self.MESSAGES['error_invalid_command'])
+                    return result, ValueError, 'error'
+        else:       
+            result.append(self.MESSAGES['error_invalid_input_data'])
+            return result, ValueError, 'error'
